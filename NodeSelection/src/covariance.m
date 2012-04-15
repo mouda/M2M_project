8bdddb9cb85b185b9ee18a72bacff26f689d51ac
mouda <dses5211@gmail.com>
2012-04-15 16:47:27 +0800

    update

---
+++ NodeSelection/src/covariance.m
@@ -0,0 +1,5 @@
+function [ covMatrix ] covariance( thetaI, thetaJ, d , a )
+%this function is used to generate the converance matrix
+
+
+end
---
+++ NodeSelection/src/main.m
@@ -0,0 +1,7 @@
+%This is the main program in the node selection simulation.
+
+%fprintf('hello\n');
+[ x, y ] = nodeGenerate(100,300);
+%disp(x);
+%disp(y);
+scatter(x,y);
