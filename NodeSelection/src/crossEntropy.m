94ed63fb3111bfb619a5848e895ffc98ad65e6ec
mouda <dses5211@gmail.com>
2012-04-21 23:43:22 +0800

    update

--- NodeSelection/src/crossEntropy.m
+++ NodeSelection/src/crossEntropy.m
@@ -1,7 +1,8 @@
-function [ solution ] = crossEntropy()
+function [ solution ] = crossEntropy( covaranceMatrix , theta, delta)
 % optimize the infromation we would like to transfer
 % and return the solution string
 
 % calculate the minimum transfer time
+t = (1/2)*log2(2*pi*exp(1).*theta/delta)/(log2(1+(G*P)/(W*N0)));
 
 end
