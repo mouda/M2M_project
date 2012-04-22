a896b8186ccbb93f2337e4022917f6b2101e36c2
mouda <dses5211@gmail.com>
2012-04-22 19:22:50 +0800

    cross entropy

--- NodeSelection/src/crossEntropy.m
+++ NodeSelection/src/crossEntropy.m
@@ -1,8 +1,40 @@
-function [ solution ] = crossEntropy( covaranceMatrix , theta, delta)
+function [ solution ] = crossEntropy( SIGMA , sigma, delta, G, P, W, N0, T)
 % optimize the infromation we would like to transfer
 % and return the solution string
 
 % calculate the minimum transfer time
-t = (1/2)*log2(2*pi*exp(1).*theta/delta)/(log2(1+(G*P)/(W*N0)));
+t = (1/2)*(log2(2*pi*exp(1)*sigma.*sigma./(delta.*delta)))./(W*log2(1+(G.*P)./(W.*N0)));
+% initialize p
+N = length(sigma);
+p(1:N) = 1/2;
+HM = 0;
+S(1:N) = 0;
+x = rand(8*N, N);
+
+pp = repmat(p,8*N,1);
+x = ceil( x - pp);
+%for i = 1 : N
+%    time = t(find( x(i) == 1);
+%     while( sum(time)  > T) 
+%         x(i,max(t)) = 0; 
+%         time = time - t
+%     end
+%end
+%size(x)
+%size(t)
+totalTime = x*transpose(t); 
+notGood = find(totalTime > T);
+for i = 1 : length(notGood)
+    while ( x(notGood(i),:)*transpose(t) > T)
+        x(notGood(i),find(t == max(x(notGood(i),:).*t),1, 'first')) = 0;
+    end
+end
+for i = 1 : 8*N
+    S = SIGMA(find(x(i)==1),find(x(i)==1))
+    
+end
+
+
+
 
 end
--- NodeSelection/src/main.m
+++ NodeSelection/src/main.m
@@ -1,23 +1,28 @@
 %This is the main program in the node selection simulation.
+N = 100;
+P(1:N)  = 0.01; 
+W = 180;%kHz
+N0 = 10^(-12);
+T = 50; %s
+delta = 2^(-8);
 
 %fprintf('hello\n');
-[ x, y ] = nodeGenerate(100,300);
-%disp(x);
-%disp(y);
-%scatter(x,y);
+[ x, y ] = nodeGenerate(N,300);
+G(1:N)  = 10^(-13.11) .* ((sqrt(x.*x + y.*y)*10^(-3)).^(-4.281));
 
 % setting up the scenraio
 % construct the distance matrix d
-for i = 1 : 100
-    for j = 1 : 100
+for i = 1 : N
+    for j = 1 : N
         d(i,j) = ((x(i) - x(j))^2 + (y(i) - y(j))^2)^(1/2);
     end
 end
 %disp(d);
 % construct the variance vector
 
-theta(1:length(x)) = 0.5;
+sigma(1:length(x)) = 0.5;
 %disp(theta);
 
-[ covar] = covariance(theta,d,2500);
-disp(theta);
+[ covar] = covariance(sigma,d,2500);
+
+crossEntropy( covar , sigma, delta, G, P, W, N0, T )
